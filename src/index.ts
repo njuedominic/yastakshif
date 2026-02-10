2e3c772ca43409c3eb80f697308b0c9bfc51d871
Dominic Njue <domnjue@Dominics-MacBook-Pro.local>
2026-02-10 20:36:20 +0200

    docs: add OpenAPI specification and Swagger UI

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import attractionsRouter from "./routes/attractions.routes";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerDocument = YAML.load("./docs/openapi.yaml");
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
+app.use("/api/v1/attractions", attractionsRouter);
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
